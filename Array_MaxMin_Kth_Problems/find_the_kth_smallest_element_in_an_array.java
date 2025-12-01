6f9599a71c185a475b809fc57aaae7d64ffe9d4c
Riya <bhandari28riya@gmail.com>
2025-12-01 17:50:32 +0530

    Add implementation for finding the Kth smallest element in an array

---
+++ Array_MaxMin_Kth_Problems/find_the_kth_smallest_element_in_an_array.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * Problem. Return the Kth smallest element.
+ *
+ * Input: nums, k
+ *
+ * Example:
+ * Input: nums = [7,10,4,3,20,15], k = 3
+ * Output: 7
+ *
+ * Constraints:
+ * 1 <= k <= nums.length
+ * Array may contain duplicates
+ *
+ * Sample test cases:
+ * [7,10,4,3,20,15], k = 3 -> 7
+ * [1], k = 1 -> 1
+ * [2,2,3,1], k = 2 -> 2
+ * [5,3,9,4,8], k = 1 -> 3
+ * [5,3,9,4,8], k = 5 -> 9
+ *
+ */
